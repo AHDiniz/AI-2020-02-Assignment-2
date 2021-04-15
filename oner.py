a10ef2fa134b0facbe15deb2bcca98c03316540f
Alan H. Diniz <alanhdiniz@gmail.com>
2021-04-15 11:43:19 -0300

    Adding struct definition to the AST and symbol table

---
+++ oner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.8
+
+import numpy as np
+from sklearn.base import BaseEstimator
+
+class OneREstimator(BaseEstimator):
+    
+    # Training the One Rule estimator:
+    def fit(self, X, Y):
+        dimensions = np.shape(X)
+        unique = np.unique(Y)
+        num_classes = len(unique)
+
+        # Creating the contingency table:
+        contingency_tables = []
+        # For each parameter:
+        for i in dimensions[1]:
+        # Create list of possible values for each parameter:
+            possible_values = np.unique(X[:i])
+            contingency_table = []
+            # For each possible value:
+            for possible_value in possible_values:
+                # Get indices of points where current parameter is equal to current possible value:
+                indices = np.where(X[:i] == possible_value)
+                # Get classes in those indices:
+                classes = np.take(Y, indices)
+                # Calculate amount of elements in each class:
+                _, contingency_list = np.unique(classes, return_counts=True)
+                contingency_table.append(contingency_list)
+            contingency_tables.append(contingency_table)
+
+        self._best_parameter = -1 # Will hold the value of the best parameter to be used in the predictions
+        best_sum = 0
+        # For each contingency table:
+        for i in range(len(contingency_tables)):
+            sum_in_paramenter = np.sum(np.apply_along_axis(lambda x: np.amax(x), 0, contingency_tables[i]))
+            if sum_in_paramenter > best_sum:
+                best_sum = sum_in_paramenter
+                self._best_parameter = i
+    
+    def predict(self, X):
+        print("Predict")
